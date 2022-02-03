d84e52a39511785031fec3e910ff176b91c8da44
Pavel Hendelman <pavel.handleman@gmail.com>
2022-02-03 12:26:16 +0300

    api functions defined

--- src/lambdas/api.js
+++ src/lambdas/api.js
@@ -5,11 +5,34 @@ const logout = require('./logout.js');
 const register = require('./register.js');
 
 // debit api
-const getMonthlydDebitList = require('./getMonthlydDebitList.js');
+const getMonthlyDebitList = require('./getMonthlyDebitList.js');
+const postMonthlyDebit = require('./postMonthlyDebit.js');
+const deleteDebit = require('./deleteDebit.js');
+
+//obligations api
+const getMonthlyObligationList = require('./getMonthlyObligationList.js');
+const postMonthlyObligation = require('./postMonthlyObligation.js');
+const deleteObligation = require('./deleteObligation.js');
+
+//credits api
+const getMonthlyCreditList = require('./getMonthlyCreditList.js');
+const postCredit = require('./postCredit.js');
+const deleteCredit = require('./deleteCredit.js');
+
 
 module.exports.login = login;
 module.exports.logout = logout;
 module.exports.register = register;
 
 
-module.exports.getMonthlydDebitList = getMonthlydDebitList;
+module.exports.getMonthlyDebitList = getMonthlyDebitList;
+module.exports.postMonthlyDebit = postMonthlyDebit;
+module.exports.deleteDebit = deleteDebit;
+
+module.exports.getMonthlyObligationList = getMonthlyObligationList;
+module.exports.postMonthlyObligation = postMonthlyObligation;
+module.exports.deleteObligation = deleteObligation;
+
+module.exports.getMonthlyCreditList = getMonthlyCreditList;
+module.exports.postCredit = postCredit;
+module.exports.deleteCredit = deleteCredit;
---
+++ src/lambdas/postCredit.js
@@ -0,0 +1,32 @@
+let response;
+
+/**
+ *
+ * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+ * @param {Object} event - API Gateway Lambda Proxy Input Format
+ *
+ * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
+ * @param {Object} context
+ *
+ * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+ * @returns {Object} object - API Gateway Lambda Proxy Output Format
+ * 
+ */
+module.exports = async (event, context) => {
+    try {
+        const { date } = event.pathParameters;
+        const { body } = event;
+        const item = JSON.parse(body);
+        response = {
+            'statusCode': 200,
+            'body': JSON.stringify({
+                message: `post credit item ${JSON.stringify(item)} api for ${new Date(date)}`,
+            })
+        }
+    } catch (err) {
+        console.log(err);
+        return err;
+    }
+
+    return response
+};
